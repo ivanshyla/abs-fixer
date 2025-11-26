c86a185fb514579c80b67b9dbf0a8ca1167b51ad
Ivan <salihorsk@gmail.com>
2025-11-26 14:42:05 +0100

    Fix: Update favicon to match branding (SVG)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,9 @@ export const metadata: Metadata = {
     images: ["/og-image.png"],
   },
   icons: {
-    icon: "/icon.png",
-    shortcut: "/icon.png",
-    apple: "/icon.png",
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
+    apple: "/icon.svg",
   },
 };
 
